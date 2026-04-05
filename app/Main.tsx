a49bfcd31eec70e0d5f4efcc03b7ba54b3047370
Costas Basdekis <costas.basdekis@gmail.com>
2026-04-05 23:48:07 +0100

    Initial commit

---
+++ app/Main.tsx
@@ -0,0 +1,9 @@
+export function Main() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9">Prefecture</header>
+      </div>
+    </main>
+  );
+}
