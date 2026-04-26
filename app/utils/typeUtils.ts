cb6129f057c9b4195703de4da6acd1bfe525b3a1
Costas Basdekis <costas.basdekis@gmail.com>
2026-04-26 16:37:47 +0100

    Fix error message for `unreachableCase`

--- app/utils/typeUtils.ts
+++ app/utils/typeUtils.ts
@@ -1,4 +1,8 @@
 export function unreachableCase(value: never, message: string): never;
-export function unreachableCase(message: string) {
+export function unreachableCase(
+  valuerOrMessage: string | never,
+  maybeMessage: string | undefined,
+) {
+  const message = maybeMessage ?? valuerOrMessage;
   throw new Error(message);
 }
